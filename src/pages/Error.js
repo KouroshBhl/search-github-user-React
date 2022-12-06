ca773a26953ef2b54662cf67b4f36a1d45b70b77
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-06 07:19:53 +0330

    Implement Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa'
 
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <div className='flex items-center justify-center flex-col gap-10 min-h-screen text-white bg-gray-800'>
+      <h1 className='text-8xl'>404</h1>
+      <h2 className='text-4xl'>page not found!</h2>
+      <div className='flex justify-center items-center px-4 py-2 bg-slate-700 rounded-lg hover:bg-slate-600 gap-2'>
+        <Link to='/'>Go back home</Link>
+        <FaHome />
+      </div>
+    </div>
+  )
 }
 
 export default Error
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans serif'],
+    },
     extend: {
       colors: {
         'hero-primary': 'rgba(53, 0, 139, 1)',
